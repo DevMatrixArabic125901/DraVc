6c087eb11e5d73f61c8f362f1a5e9a57f34b635b
drago <119659334+qithoniq@users.noreply.github.com>
2023-08-17 05:35:25 +0300

    Update vcp_helper.py

--- helper/vcp_helper.py
+++ helper/vcp_helper.py
@@ -56,7 +56,7 @@ class dravc:
         try:
             await self.app.join_group_call(
                 chat_id=chat.id,
-                stream=AudioPiped("dragovc/resources/Silence01s.mp3"),
+                stream=AudioPiped("DraVc/resources/Silence01s.mp3")
                 join_as=join_as_chat,
                 stream_type=StreamType().pulse_stream,
             )
