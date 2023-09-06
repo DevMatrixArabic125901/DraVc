ef94424fedb3c61f693ca8b593f4194e556c8673
drago <119659334+qithoniq@users.noreply.github.com>
2023-09-06 23:44:35 +0300

    Update vcp_helper.py

--- helper/vcp_helper.py
+++ helper/vcp_helper.py
@@ -43,13 +43,13 @@ class dravc:
 
     async def join_vc(self, chat, join_as=None):
         if self.CHAT_ID:
-            return f"موجود بالفعل في المكالمة الصوتية {self.CHAT_NAME}"
+            return f"- مـوجـود بالفعـل بالمحـادثـه الصـوتيـه عـلى {self.CHAT_NAME}"
         if join_as:
             try:
                 join_as_chat = await self.client.get_entity(int(join_as))
-                join_as_title = f" على **{join_as_chat.title}**"
+                join_as_title = f" كـ **{join_as_chat.title}**"
             except ValueError:
-                return "عليك كتابة ايدي الدردشة للأنضمام"
+                return "**- قم باضافة ايدي المجموعه لامر الانضمام**"
         else:
             join_as_chat = await self.client.get_me()
             join_as_title = ""
@@ -65,21 +65,21 @@ class dravc:
                 await self.client(
                     functions.phone.CreateGroupCallRequest(
                         peer=chat,
-                        title="دراكو",
+                        title="MATRIX VC",
                     )
                 )
                 await self.join_vc(chat=chat, join_as=join_as)
             except ChatAdminRequiredError:
-                return "- عليك ان تكون مشرف في الدردشة اولا"
+                return "انت بحاجـه الى صلاحيـات المشـرف لبـدء محـادثه صـوتيـه, او قم بطلـب من احـد المشـرفين"
         except (NodeJSNotInstalled, TooOldNodeJSVersion):
-            return "- عليك تثبيت المتطلبات اولا شاهاد القناة الاساسية @drago"
+            return "- آخـر اصـدار من NodeJs لم يتـم تحميلـه ...؟!"
         except AlreadyJoinedError:
             await self.app.leave_group_call(chat.id)
             await asyncio.sleep(3)
             await self.join_vc(chat=chat, join_as=join_as)
         self.CHAT_ID = chat.id
         self.CHAT_NAME = chat.title
-        return f"- تم الانضمام الى الدردشة : **{chat.title}**{join_as_title}"
+        return f"**- تم الانضمـام بنجـاح الى المحادثـه الصـوتيـه** **{chat.title}**{join_as_title}"
 
     async def leave_vc(self):
         try:
@@ -99,13 +99,13 @@ class dravc:
             if title:
                 playable = await video_dl(input, title)
             else:
-                return "خطأ اثناء التعرف على الرابط"
+                return "- خطـأ بجلب الرابـط"
         elif check_url(input):
             try:
                 res = requests.get(input, allow_redirects=True, stream=True)
                 ctype = res.headers.get("Content-Type")
                 if "video" not in ctype or "audio" not in ctype:
-                    return "الرابط غير صحيح"
+                    return "- رابـط غيـر صـالح ؟!"
                 name = res.headers.get("Content-Disposition", None)
                 if name:
                     title = name.split('="')[0].split('"') or ""
@@ -113,32 +113,32 @@ class dravc:
                     title = input
                 playable = input
             except Exception as e:
-                return f"الرابط غير صحيح\n\n{e}"
+                return f"**- رابـط غيـر صـالح :**\n\n{e}"
         else:
             path = Path(input)
             if path.exists():
                 if not path.name.endswith(
                     (".mkv", ".mp4", ".webm", ".m4v", ".mp3", ".flac", ".wav", ".m4a")
                 ):
-                    return "- هذا الملف غير صحيح ليتم تشغيله"
+                    return "ملف غيـر صـالح لتشغيـله"
                 playable = str(path.absolute())
                 title = path.name
             else:
-                return "مسار الملف غير صحيح"
+                return "مسـار الملـف غيـر موجـود ؟!"
         print(playable)
         if self.PLAYING and not force:
             self.PLAYLIST.append({"title": title, "path": playable, "stream": stream})
-            return f"- تمت اضافته الى قائمة التشغيل.\n الموقع: {len(self.PLAYLIST)+1}"
+            return f"- تم الاضـافه لـ قـائمـة التشغيـل ✓\n- المـوقـع: {len(self.PLAYLIST)+1}"
         if not self.PLAYING:
             self.PLAYLIST.append({"title": title, "path": playable, "stream": stream})
             await self.skip()
-            return f"يتم تشغيل {title}"
+            return f"- جـارِِ تشغيـل {title}"
         if force and self.PLAYING:
             self.PLAYLIST.insert(
                 0, {"title": title, "path": playable, "stream": stream}
             )
             await self.skip()
-            return f"يتم تشغيل {title}"
+            return f"- جـارِ تشغيـل {title}"
 
     async def handle_next(self, update):
         if isinstance(update, StreamAudioEnded):
@@ -155,7 +155,7 @@ class dravc:
                     AudioPiped("DraVc/resources/Silence01s.mp3"),
                 )
             self.PLAYING = False
-            return "- تم تخطي التشغيل الحالي\nقائمة التشغيل فارغة"
+            return "- التخطـي:\nقائمـة الشغيـل فارغـه ؟!"
 
         next = self.PLAYLIST.pop(0)
         if next["stream"] == Stream.audio:
@@ -167,20 +167,36 @@ class dravc:
         except Exception:
             await self.skip()
         self.PLAYING = next
-        return f"- تم تخطي التشغيل الحالي\nيتم تشغيل : `{next['title']}`"
+        return f"- تم التخطي\n- جـارِ تشغيـل : `{next['title']}`"
 
     async def pause(self):
         if not self.PLAYING:
-            return "- لم يتم تشغيل شي لأيقافه"
+            return "لايـوجـد شـي لـ الايقـاف ؟!"
         if not self.PAUSED:
             await self.app.pause_stream(self.CHAT_ID)
             self.PAUSED = True
-        return f"- تم الايقاف المؤقت في {self.CHAT_NAME}"
+        return f"تم التمهـل في {self.CHAT_NAME}"
 
     async def resume(self):
         if not self.PLAYING:
-            return "- لم يتم تشغيل شي لأستأنافه"
+            return "لايـوجـد شـي لـ الاستئنـاف ؟!"
         if self.PAUSED:
             await self.app.resume_stream(self.CHAT_ID)
             self.PAUSED = False
-        return f"- تم الاستئناف في {self.CHAT_NAME}"
+        return f"تم الاستئنـاف في {self.CHAT_NAME}"
+
+    # async def mute(self):
+    #     if not self.PLAYING:
+    #         return "Nothing is playing to Mute"
+    #     if not self.MUTED:
+    #         await self.app.mute_stream(self.CHAT_ID)
+    #         self.PAUSED = True
+    #     return f"Muted Stream on {self.CHAT_NAME}"
+
+    # async def unmute(self):
+    #     if not self.PLAYING:
+    #         return "Nothing is playing to Unmute"
+    #     if self.MUTED:
+    #         await self.app.unmute_stream(self.CHAT_ID)
+    #         self.MUTED = False
+    #     return f"Unmuted Stream on {self.CHAT_NAME}"
